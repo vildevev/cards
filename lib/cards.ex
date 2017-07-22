24831bd0c44e3031b22131390711621bf259eae7
“Vilde <vilde@vevatne.no>
2017-07-21 20:24:59 -0700

    Add contains? method to Cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,7 +6,14 @@ defmodule Cards do
   end
 
   def shuffle(deck) do
-    
+    # No need to import Edum module
+    # Data structures are immutable 
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    # Question mark implies that the function return a boolean
+    Enum.member?(deck, card)
   end
 
 end
