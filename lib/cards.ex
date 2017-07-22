a3a8e02b17a7bdd2bf003e83aeea9f963b932455
“Vilde <vilde@vevatne.no>
2017-07-21 20:34:42 -0700

    Rewrite create_deck function with list comprenhension

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,7 +2,13 @@ defmodule Cards do
 
   def create_deck do
     # Double quotes is convention
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    # List comprehension:
+    # Is a map function. Returns new array.
+    for suit <- suits do 
+      suit
+    end
   end
 
   def shuffle(deck) do
@@ -13,6 +19,7 @@ defmodule Cards do
 
   def contains?(deck, card) do
     # Question mark implies that the function return a boolean
+    # Function isn't called on array as in OO Programming. Is instead a 'master' function that can take the object as one of the arguments.
     Enum.member?(deck, card)
   end
 
