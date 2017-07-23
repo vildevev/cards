cc3f7c0eb3bdec673408c9fab2086b7b8cecf9c6
“Vilde <vilde@vevatne.no>
2017-07-22 19:26:21 -0700

    Add function docs with concrete example on how to user create_deck function

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,6 +3,9 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     # Double quotes is convention
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -27,6 +30,17 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+      
+  """
   def deal(deck, hand_size) do
     # Whenever possible: delegate to built-in methods 
     # List = linked-list, Tuple = Hash
