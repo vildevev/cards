4d7dbb4e4eb437295670838f2b19daa78107bb5f
“Vilde <vilde@vevatne.no>
2017-07-22 20:59:29 -0700

    Add test for shuffle method

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,5 +2,18 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  # Doc testing: automatically generated from doc examples.
+  # Doctests: automatically generated from doc examples.
+  # Case tests: assert(true), refute(false) keyword next to expression that evaluates to a boolean.
+  # Test behaviors that you care about in document.
+
+  test "create_deck makes 20 cards" do
+  	deck_length = length(Cards.create_deck)
+  	assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+  	deck = Cards.create_deck
+  	assert deck != Cards.shuffle(deck)
+  end
+
 end
